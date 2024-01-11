a6702f9376ef90ed3e77e3f6f7bef07802ad330a
Landon <landoncramer1@gmail.com>
2024-01-11 12:43:19 -0800

    update

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/landoncramer/Development/code/practice/deploy-ShoeHaven2.0/ShoeHaven2.0/myenv"
+set -gx VIRTUAL_ENV "/Users/landoncramer/Development/code/practice/deploy-ShoeHaven2.0/D_ShoeHaven2.0/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- server/config.py
+++ server/config.py
@@ -25,6 +25,7 @@ app = Flask(
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.json.compact = False
 
 # Define metadata, instantiate db
